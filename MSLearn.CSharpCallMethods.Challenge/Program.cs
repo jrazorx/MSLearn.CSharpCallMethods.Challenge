d8d070b73af427a9d795c4f1c614b321ac21d870
jra <julien.gault.50@gmail.com>
2024-02-19 14:11:38 +0100

    Starting point for the challenge

--- MSLearn.CSharpCallMethods.Challenge/Program.cs
+++ MSLearn.CSharpCallMethods.Challenge/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+Console.WriteLine(largerValue);
